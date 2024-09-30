ee0d1126b73c6edef77a7c6584e9b82bb86e4337
Carlos <chn-neto@hotmail.com>
2024-09-30 15:08:39 -0400

    Adição da foto

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,8 @@
 import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import Sidebar from "./components/sidebar/page";
+import Image from "next/image";
+import Model from "./public/images/foto.png"
 
 export default function Home() {
 
@@ -9,10 +11,20 @@ export default function Home() {
         <div>
 <Sidebar/>
 <div className="ml-64 p-4">
-    <div className="text-4xl text-center font-bold">
+    <div className=" display-5 text-4xl text-center font-bold">
     
         Catalogo Filmes
     </div>
+    <div style={{
+          display: "flex",
+          justifyContent: "center",
+        }}>
+
+    <Image src={Model} width={450}
+      height={450}/>
+    </div>
+    
+    
 
 </div>
 
